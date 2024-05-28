7b2972ddc8e78fda5ed77a73232ed6405ab7528e
Amwaj <amwaj.1431@outlook.com>
2024-05-28 03:44:09 +0300

    v1

---
+++ script.js
@@ -0,0 +1,7 @@
+let nav = document.getElementsByClassName("dropdown-content")[0];
+
+function navBar() {
+  if (nav.style.display == "block") {
+    nav.style.display = "none";
+  } else nav.style.display = "block";
+}
